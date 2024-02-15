8a5d014c316d576dd1d68a556cbd1f5183a3ec99
root <amaltsev77@ya.ru>
2024-02-15 08:38:41 +0700

    удалено лишнее

--- diagnostics/src/diagnostics.c
+++ diagnostics/src/diagnostics.c
@@ -12,49 +12,8 @@
 
 #include <assert.h>
 
-/* Type of interface implementing object. */
-typedef struct IModeImpl {
-    struct traffic_light_IMode base;     /* Base interface of object */
-    rtl_uint32_t step;                   /* Extra parameters */
-} IModeImpl;
 
-/* Mode method implementation. */
-static nk_err_t FMode_impl(struct traffic_light_IMode *self,
-                          const struct traffic_light_IMode_FMode_req *req,
-                          const struct nk_arena *req_arena,
-                          traffic_light_IMode_FMode_res *res,
-                          struct nk_arena *res_arena)
-{
-    IModeImpl *impl = (IModeImpl *)self;
-    /**
-     * Increment value in control system request by
-     * one step and include into result argument that will be
-     * sent to the control system in the lights gpio response.
-     */
-    res->result = req->value + impl->step;
-    return NK_EOK;
-}
 
-/**
- * IMode object constructor.
- * step is the number by which the input value is increased.
- */
-static struct traffic_light_IMode *CreateIModeImpl(rtl_uint32_t step)
-{
-    /* Table of implementations of IMode interface methods. */
-    static const struct traffic_light_IMode_ops ops = {
-        .FMode = FMode_impl
-    };
-
-    /* Interface implementing object. */
-    static struct IModeImpl impl = {
-        .base = {&ops}
-    };
-
-    impl.step = step;
-
-    return &impl.base;
-}
 
 /* Diagnostics entry point. */
 int main(void)
